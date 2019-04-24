561093210448c31a5e0ac2a22559df22b66a8a5f
Lucas <lucas-sena>
2019-04-24 00:11:42 -0300

    Criando funcao de cadastro de novos usuarios.

--- Projeto_Unidade_1.py
+++ Projeto_Unidade_1.py
@@ -27,10 +27,22 @@ def verifica(id, senha):
     pessoas.close()
 
 
+def cadastro():
+    pessoas = open('dataBase.txt','a')
+
+    base = ['Id','Senha','Nome','Data de Nascimento','CPF']
+    novaPessoa = []
+    for x in range(5):
+        novaPessoa += [input('{}: '.format(base[x]))]
+
+    novaPessoaString = ','.join(novaPessoa)
+    pessoas.write(novaPessoaString)
+    pessoas.close()
+
 escolha = menu()
 if escolha == 1:
     id = input('Id: ')
     senha = input('Senha: ')
     verifica(id, senha)
 elif escolha == 2:
-    print('Not implemented yet')
+    cadastro()
